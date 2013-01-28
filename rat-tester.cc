b1f06886d87690b5742666faece5411db7214d81
Ubuntu <ubuntu@ip-10-244-85-146.us-west-2.compute.internal>
2013-01-28 09:44:20 +0000

    Expand optimization range and # of interior samples

--- rat-tester.cc
+++ rat-tester.cc
@@ -42,8 +42,8 @@ int main( int argc, char *argv[] )
   }
 
   Evaluator::ConfigRange configuration_range;
-  configuration_range.link_packets_per_ms = make_pair( 1, 2 ); /* 10 Mbps to 20 Mbps */
-  configuration_range.rtt_ms = make_pair( 100, 200 ); /* ms */
+  configuration_range.link_packets_per_ms = make_pair( .5, 5 ); /* 10 Mbps to 20 Mbps */
+  configuration_range.rtt_ms = make_pair( 50, 500 ); /* ms */
   configuration_range.max_senders = 4;
   RatBreeder breeder( configuration_range );
 
