599f6cada08cc26789b920136a2c0ca1c4890aff
Ubuntu <ubuntu@ip-10-244-85-146.us-west-2.compute.internal>
2013-01-29 11:16:58 +0000

    Different power of 10

--- whisker.hh
+++ whisker.hh
@@ -51,7 +51,7 @@ public:
 };
 
 static const unsigned int MAX_WINDOW = 512;
-static constexpr double MAX_INTERSEND = 100;
+static constexpr double MAX_INTERSEND = 64;
 static constexpr double MAX_MULTIPLE = 1.0;
 
 static const unsigned int MAX_WINDOW_INCR = 64;
@@ -61,8 +61,8 @@ static constexpr double MAX_MULTIPLE_INCR = 0.5;
 static const unsigned int DEFAULT_WINDOW = 1;
 static const unsigned int DEFAULT_MULTIPLE = 1;
 
-static constexpr double MIN_INTERSEND = 0.01;
-static constexpr double MULTIPLE_INCR = 0.01;
+static constexpr double MIN_INTERSEND = 0.1;
+static constexpr double MULTIPLE_INCR = 0.05;
 
-static constexpr double INTERSEND_INCR = 0.01;
+static constexpr double INTERSEND_INCR = 0.1;
 #endif
