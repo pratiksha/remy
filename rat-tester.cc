67ccc037298f98928e463c1734d74abdc7553f4c
Ubuntu <ubuntu@ip-10-244-85-146.us-west-2.compute.internal>
2013-01-29 14:47:28 +0000

    Expand range back to 100 Mbps

--- rat-tester.cc
+++ rat-tester.cc
@@ -42,7 +42,7 @@ int main( int argc, char *argv[] )
   }
 
   Evaluator::ConfigRange configuration_range;
-  configuration_range.link_packets_per_ms = make_pair( .1, 1 ); /* 10 Mbps to 20 Mbps */
+  configuration_range.link_packets_per_ms = make_pair( .1, 10 ); /* 10 Mbps to 20 Mbps */
   configuration_range.rtt_ms = make_pair( 10, 100 ); /* ms */
   configuration_range.max_senders = 4;
   RatBreeder breeder( configuration_range );
